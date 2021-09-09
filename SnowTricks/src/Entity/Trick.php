ac41897026572d80be237e72b81208c164e6b11d
Nathan Gracia <nathan.gracia.863@gmail.com>
2021-09-09 20:54:43 +0200

    slug

--- SnowTricks/src/Entity/Trick.php
+++ SnowTricks/src/Entity/Trick.php
@@ -6,6 +6,7 @@ use App\Repository\TrickRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass=TrickRepository::class)
@@ -54,6 +55,12 @@ class Trick
      * @ORM\JoinColumn(nullable=false)
      */
     private $trickGroup;
+
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
 
     public function __construct(){
         $this->created_at = new \DateTime();
@@ -204,4 +211,10 @@ class Trick
 
         return $this;
     }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
 }
